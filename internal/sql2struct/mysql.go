b0f384cd4953cba7852a69f61d931469010b2d06
mier <1158737789@qq.com>
2021-08-08 23:06:39 +0800

    mysql

--- internal/sql2struct/mysql.go
+++ internal/sql2struct/mysql.go
@@ -1,30 +1,52 @@
 package sql2struct
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 type DBModel struct {
 	DBEngine *sql.DB
-	DBInfo *DBInfo
+	DBInfo   *DBInfo
 }
 
-type DBInfo struct{
-	DBType string
-	Host string
+type DBInfo struct {
+	DBType   string
+	Host     string
 	UserName string
 	Password string
-	Charset string
+	Charset  string
 }
 
-type TableColumn struct{
-	ColumnName string
-	DataType string
-	IsNullable string
-	ColumnKey string
-	ColumnType string
+type TableColumn struct {
+	ColumnName    string
+	DataType      string
+	IsNullable    string
+	ColumnKey     string
+	ColumnType    string
 	ColumnComment string
 }
 
-func NewDBModel(Info *DBInfo) *DBModel{
+func NewDBModel(Info *DBInfo) *DBModel {
 	return &DBModel{
+		DBInfo: Info,
 	}
+}
+
+func (m *DBModel) Connect() error {
+	var err error
+	s := "%s:%s@tcp(%s)/information_schema?" + "charset=%s&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(
+		s,
+		m.DBInfo.UserName,
+		m.DBInfo.Password,
+		m.DBInfo.Host,
+		m.DBInfo.Charset,
+	)
+	m.DBEngine, err = sql.Open(m.DBInfo.DBType, dsn)
+	if err != nil {
+		return err
+	}
+	return nil
 }
